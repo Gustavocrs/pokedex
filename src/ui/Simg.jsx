bd88c3348be84e7a9c54a69c84c8c17f1222a573
Gustavocrs <86523009+Gustavocrs@users.noreply.github.com>
2022-08-20 15:33:05 -0300

    correção de responsividade CardPokemonFoto

--- src/ui/Simg.jsx
+++ src/ui/Simg.jsx
@@ -2,8 +2,20 @@ import styled from "styled-components";
 
 export const Simg = styled.img`
   width: auto;
-  max-width: 200px;
+  max-width: 300px;
   height: 50vh;
   margin: 15px 0;
   filter: drop-shadow(0 0 10px #f1f1f1);
+  transition: 500ms;
+
+  @media screen and (max-height: 700px) {
+    max-width: 200px;
+    transition: 500ms;
+  }
+
+  @media screen and (max-height: 460px) {
+    display: none;
+    transition: 500ms;
+  }
+
 `;
