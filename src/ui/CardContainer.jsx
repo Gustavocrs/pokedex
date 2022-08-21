2bdada83a074f70adc5d243737a769d1bc108853
Gustavocrs <86523009+Gustavocrs@users.noreply.github.com>
2022-08-20 21:07:04 -0300

    update CardContainer

--- src/ui/CardContainer.jsx
+++ src/ui/CardContainer.jsx
@@ -3,11 +3,19 @@ import styled from "styled-components";
 export const CardContainer = styled.section`
   display: flex;
   flex-direction: column;
-  justify-content: space-evenly;
+  justify-content: flex-start;
   align-items: center;
   height: 100vh;
   width: 100%;
   overflow: hidden;
+
+  @media screen and (max-width: 245px) {
+    min-height: 240px;
+  }
+
+  @media screen and (max-width: 620px) {
+    justify-content: center;
+  }
 `;
 
 export const CardContainerBase = styled(CardContainer)`
